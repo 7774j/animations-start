5060a10c70a46c39913976b2e91204b606495a97
7774j <80786885+7774j@users.noreply.github.com>
2021-07-04 19:23:28 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { state, style, trigger, transition, animate } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState',[
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)',
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      //transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState',[
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange',
+          borderRadius: '0px'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ])
+  ]
+})
+export class AppComponent {
+  state = 'normal';
+  wildState = 'normal';
+
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAnimate(){
+      this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+      this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+    }
+    onShrink(){
+      this.wildState = 'shrunken';
+    }
+
+    onAdd(item) {
+      this.list.push(item);
+    }
+}
